4d99aa2f4f39bd2b06157bdb08442fde85d4011b
Anthony <adavid16967@gmail.com>
2023-02-13 23:01:21 -0500

    more styling for header component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import './Header.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCalendarDays, faCar, faPerson, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
 	return (
 		<div className="header">
 			<div className='headerContainer'>
-				 <div className="headerListItem">
+        <div className="headerList">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
           </div>
@@ -26,6 +27,30 @@ function Header() {
             <FontAwesomeIcon icon={faTaxi} />
             <span>Airport taxis</span>
           </div>
+        </div>
+        <h1 className="headerTtitle">A lifetime of discounts? It's Genius</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels - unlock instant savings for 10% or more!
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input 
+              type="text" 
+              placeholder='Where are you going?'
+              className='headrerSearchInput'
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+            <span>date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span>2 Adults 2 Children 1 Room</span>
+          </div>
+        </div>
 			</div>     
     </div>
 	)
