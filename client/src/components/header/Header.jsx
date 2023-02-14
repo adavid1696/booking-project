20640c86ee56a2a825b455c6e4523d6f08c8bf80
Anthony <adavid16967@gmail.com>
2023-02-13 22:27:15 -0500

    boilerplater for booking app

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+
+function Header() {
+	return (
+		<div className="header">
+			<div className='headerContainer'>
+				 <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
+          </div>
+			</div>     
+    </div>
+	)
+}
+
+export default Header
