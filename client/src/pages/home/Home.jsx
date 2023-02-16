a2f5338ba9d4d5fdcbb6b6175179266d803aebf8
Anthony <adavid16967@gmail.com>
2023-02-16 15:09:38 -0500

    propertylist added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
 import './Home.css'
 
 function Home() {
@@ -10,9 +11,10 @@ function Home() {
 			<Header />
 			<div className="homeContainer">
 				<Featured />
-				<h1 className="homeTitle">
-					Browse by property type
-				</h1>
+				<h1 className="homeTitle">Browse by property type</h1>
+				<PropertyList />
+				<h1 className="homeTitle">Browse by featured type</h1>
+				
 			</div>
 		</div>
 	)
