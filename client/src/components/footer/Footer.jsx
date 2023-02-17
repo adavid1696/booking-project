1ecfe993c34c8fa799052a1b0715ee41be883c9f
Anthony <adavid16967@gmail.com>
2023-02-16 19:41:40 -0500

    footer done

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import './footer.css'
+
+function Footer() {
+	return (
+		<div className="footer">
+			<div className="fLists">
+				<ul className="flist">
+					<li className="fListItem">sdfgerg</li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+				</ul>
+				<ul className="flist">
+					<li className="fListItem">sdfgerg</li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+				</ul>
+				<ul className="flist">
+					<li className="fListItem">sdfgerg</li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+				</ul>
+				<ul className="flist">
+					<li className="fListItem">sdfgerg</li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+				</ul>
+				<ul className="flist">
+					<li className="fListItem">sdfgerg</li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+					<li className="fListItem"></li>
+				</ul>
+			</div>
+			
+		</div>
+	)
+}
+
+export default Footer
